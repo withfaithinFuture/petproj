344e130d0ab2d14a923ec72f85fb80c5320f20b8
withfaithinFuture <110636640+withfaithinFuture@users.noreply.github.com>
2025-12-19 21:47:36 +0300

    Delete src directory

--- src/app/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import asyncio
-
-
-async def main() -> None:
-    uvicorn.run('application:get_app', host='localhost', port=8000, reload=True, factory=True)
-
-if __name__ == '__main__':
-    asyncio.run(main())
